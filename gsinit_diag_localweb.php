d5f28271803bdd9f37599e9acf1b6775237cce2d
tyler retzlaff <tyler@speartech.org>
2026-03-18 22:48:09 -0400

    fixed server ip replacement in gsinit_diag_localweb.php

--- gsinit_diag_localweb.php
+++ gsinit_diag_localweb.php
@@ -1,42 +1,42 @@
 [Servers]
-RouterIP0=192.168.0.213
+RouterIP0=172.210.68.184
 RouterPort0=40000
 RouterLauncherPort0=40005
-RouterIP1=192.168.0.213
+RouterIP1=172.210.68.184
 RouterPort1=40000
 RouterLauncherPort1=40005
-IRCIP0=192.168.0.213
+IRCIP0=172.210.68.184
 IRCPort0=6668
-ProxyIP0=192.168.0.213
+ProxyIP0=172.210.68.184
 ProxyPort0=44001
-CDKeyServerIP0=192.168.0.213
+CDKeyServerIP0=172.210.68.184
 CDKeyServerPort0=44000
-NATServerIP0=192.168.0.213
+NATServerIP0=172.210.68.184
 NATServerPort0=45000
-NATServerIP1=192.168.0.213
+NATServerIP1=172.210.68.184
 NATServerPort1=45000
-NATServerIP2=192.168.0.213
+NATServerIP2=172.210.68.184
 NATServerPort2=45000
-NATServerIP3=192.168.0.213
+NATServerIP3=172.210.68.184
 NATServerPort3=45000
 
 [AutoUpdate]
-AutoUpdateURL=http://192.168.0.213/gsupdate/
+AutoUpdateURL=http://172.210.68.184/gsupdate/
 
 [DynamicContent]
-ContentDownload=http://192.168.0.213/content.php?username=%s&version=%s&gamename=%s&language=%s
-HelpURL=http://192.168.0.213/gshelp/
-LogoURL=http://192.168.0.213/ubisoft/
-PlayNowURL=http://192.168.0.213/gamingzone/index.asp?l=%s
-RankingURL=http://192.168.0.213/ladder/index.asp?gamename=%s&ladderid=%s&lan=%s
-ShopURL=http://192.168.0.213/shop/
-WhosOnURL=http://192.168.0.213/whoson/index.asp
+ContentDownload=http://172.210.68.184/content.php?username=%s&version=%s&gamename=%s&language=%s
+HelpURL=http://172.210.68.184/gshelp/
+LogoURL=http://172.210.68.184/ubisoft/
+PlayNowURL=http://172.210.68.184/gamingzone/index.asp?l=%s
+RankingURL=http://172.210.68.184/ladder/index.asp?gamename=%s&ladderid=%s&lan=%s
+ShopURL=http://172.210.68.184/shop/
+WhosOnURL=http://172.210.68.184/whoson/index.asp
 
 [Games]
-GamesDataURL=http://192.168.0.213/en/games/
-GamesURL=http://192.168.0.213/en/games.ini
-GS4GamesDataURL=http://192.168.0.213/gs4/
-GS4GamesURL=http://192.168.0.213/Games.ini
+GamesDataURL=http://172.210.68.184/en/games/
+GamesURL=http://172.210.68.184/en/games.ini
+GS4GamesDataURL=http://172.210.68.184/gs4/
+GS4GamesURL=http://172.210.68.184/Games.ini
 
 [Lobbies]
 LobbyNames=lobby-gsalobby01,lobby-gsalobby02,lobby-gsalobby03,lobby-gsalobby04,lobby-gsalobby05,lobby-gsalobby06,lobby-gsalobby09
@@ -44,14 +44,14 @@ LobbyNames=lobby-gsblobby01,lobby-gsblobby02,lobby-gsblobby03,lobby-gsblobby04,l
 LobbyNames=lobby-gsclobby01
 
 [Maintenance]
-CreateAccount=http://192.168.0.213/login/CreateUser.aspx?lang=%s&NextUrl=http://splintercell4.ubi.com&skin=splintercell4
-ForgotPasswordURL=http://192.168.0.213/Login/US/ForgetPassword.htm?l=%s
-GS4ModifyAccountURL=http://192.168.0.213/Login/US/Login.htm?edacc=2&username=%s&l=%s
-GS4UserAccountURL=http://192.168.0.213/Login/US/NewUser.htm?l=%s
-PatchURL=http://192.168.0.213/RTPatches/
+CreateAccount=http://172.210.68.184/login/CreateUser.aspx?lang=%s&NextUrl=http://splintercell4.ubi.com&skin=splintercell4
+ForgotPasswordURL=http://172.210.68.184/Login/US/ForgetPassword.htm?l=%s
+GS4ModifyAccountURL=http://172.210.68.184/Login/US/Login.htm?edacc=2&username=%s&l=%s
+GS4UserAccountURL=http://172.210.68.184/Login/US/NewUser.htm?l=%s
+PatchURL=http://172.210.68.184/RTPatches/
 
 [Message]
 Message=Welcome to Splinter Cell Online! Server is up and running. Visit www.splintercellonline.net for updates.
 
 [PLUGINS]
-PluginListURL=http://192.168.0.213/GLPlugins.xml
+PluginListURL=http://172.210.68.184/GLPlugins.xml
