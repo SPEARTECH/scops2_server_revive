8cdd08f00b20d261631fad357f2ae5f398c2d514
tyler retzlaff <tyler@speartech.org>
2026-03-19 17:32:56 -0400

    added stackscript;

---
+++ stackscript.sh
@@ -0,0 +1,28 @@
+# replace ip in gsinit_diag_weblocal.php
+# create pat token
+# allow for repos and all perms
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+sudo apt-get install --update
+sudo apt-get install --upgrade
+sudo apt-get install python3-pip
+sudo apt install python3.12-venv
+python3 -m venv venv
+cd venv/bin
+source activate
+cd ../..
+python3 -m pip install -r requirements.txt
+sudo chmod 655 run.sh
+
+
+
+# Kill everything and restart clean
+# sudo pkill -f ubigs_router
+# sudo pkill -f gs_http_server
+# sudo pkill -f dns_override
+# sudo pkill -f udp_reply
+# sudo pkill -f udp_log
+# sudo pkill -f tcp_log
+# sudo pkill -f s_server
+# sleep 1
+sudo env "PATH=$PWD/venv/bin:$PATH" ./run.sh
